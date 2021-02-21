149e51870e9aaf6f0924ed67496b032b9a6376f9
taku-0728 <taku8270@gmail.com>
2021-02-21 21:22:25 +0900

    [add] hour temperature

--- WeatherReport.py
+++ WeatherReport.py
@@ -2,10 +2,14 @@ import requests
 from bs4 import BeautifulSoup as bs4
 import re
 
-urlName = "https://tenki.jp/forecast/3/14/4310/11203/1hour.html"
+urlName = "https://tenki.jp/forecast/3/16/4410/13103/1hour.html"
 url = requests.get(urlName)
 soup = bs4(url.content, 'lxml')
 
-weathers = soup.find_all(class_='weather')
+hour = soup.find(class_='hour')
+weather = soup.find(class_='weather')
+temperature = soup.find(class_='temperature')
 
-print(weathers)
+print(hour)
+print(weather)
+print(temperature)
