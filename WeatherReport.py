9b1b548494735fbff0896834d9fb4a390d600b0d
taku-0728 <taku8270@gmail.com>
2021-03-12 00:37:44 +0900

    [mod] yahoo天気APIを用いる方法に修正

--- WeatherReport.py
+++ WeatherReport.py
@@ -1,6 +1,8 @@
 import chromedriver_binary
 import requests
 import time
+import urllib
+import json
 from bs4 import BeautifulSoup as bs4
 from janome.tokenizer import Tokenizer
 from selenium import webdriver
@@ -19,69 +21,38 @@ def setPlace(text):
 
     return place
 
-def getUrl(place):
-    options = Options()
-    # ヘッドレスモードを有効にする(無効にすると画面が表示される)
-    options.add_argument('--headless')
+# 場所から緯度経度を取得
+def getLatitude(place):
 
-    # WebDriver(ブラウザ)の起動
-    driver = webdriver.Chrome(options=options)
-    driver_path = 'https://tenki.jp/'
-    driver.get(driver_path)
+    url = 'http://www.geocoding.jp/api/?q=' + place
 
-    # コンソールから入力された場所をテキストボックスに入力
-    element = driver.find_element_by_id("keyword")
-    element.send_keys(place)
-
-    # 検索ボタン押下
-    btn = driver.find_element_by_id("btn")
-    btn.click()
-
-    # 画面描画、負荷軽減用に3秒待つ
-    time.sleep(3)
-
-    # 検索結果の一覧を取得
-    searchResults = driver.find_elements_by_class_name('search-entry-data')
-
-    # 1番目の検索結果のリンクをクリック
-    searchResults[0].click()
-
-    # 画面描画、負荷軽減用に3秒待つ
-    time.sleep(3)
-
-    # 1時間ごとの天気を表示するためのリンクを選択し、クリック
-    onehour = driver.find_elements_by_class_name('forecast-select-1h')
-    onehour[0].click()
-
-    # 画面描画、負荷軽減用に3秒待つ
-    time.sleep(3)
-
-    # スクレイピング用に現在のURLを取得
-    currentUrl = driver.current_url
-
-    # ブラウザの自動操作終了
-    driver.quit()
-
-    return currentUrl
-
-def getWeatherReport(url):
     # スクレイピング用にURLを指定
     url = requests.get(url)
     soup = bs4(url.content, 'lxml')
 
-    # 現在時刻以降の時間を取得
-    hours = soup.select_one('tr.hour')
-    hours = hours.select('span:not(.past)')
+    if soup.find('error'):
+        return '', ''
 
-    # 現在時刻以降の天気を取得
-    weathers = soup.select_one('tr.weather')
-    weathers = weathers.select('p:not(.past)')
+    lat = soup.find('lat').string
+    lon = soup.find('lng').string
 
-    # 現在時刻以降の気温を取得
-    temperatures = soup.select_one('tr.temperature')
-    temperatures = temperatures.select('span:not(.past)')
+    # 緯度経度取得APIの規定で10秒待つ
+    time.sleep(5)
 
-    return hours, weathers, temperatures
+    return lat, lon
+
+# 緯度経度から天気予報を取得
+def getWeatherReport(lat, lon):
+    APP_ID = '********************************'
+    BASE_URL = 'https://map.yahooapis.jp/weather/V1/place?coordinates='
+
+    url = BASE_URL + lat + ',' + lon + '&output=json&appid=' + APP_ID
+
+    tmp = urllib.request.urlopen(url).read()
+
+    json_tree = json.loads(tmp)
+
+    print(json_tree)
 
 def main():
     print('どこの天気を知りたい？')
@@ -91,20 +62,9 @@ def main():
     # 入力されたテキストから場所を取得
     place = setPlace(text)
 
-    # 場所からスクレイピング対象のページのURLを取得
-    currentUrl = getUrl(place)
+    lat, lon = getLatitude(place)
 
-    # 取得したURLから天気情報を取得
-    hours, weathers, temperatures = getWeatherReport(currentUrl)
-
-    for hour in hours:
-        print(hour.get_text())
-
-    for weather in weathers:
-        print(weather.get_text())
-
-    for temperature in temperatures:
-        print(temperature.get_text())
+    WeatherReport = getWeatherReport(lat, lon)
 
 if __name__ == '__main__':
     main()
