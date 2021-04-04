5de3de967943b611d145417d2cff8232d8603603
taku-0728 <taku8270@gmail.com>
2021-04-04 23:58:42 +0900

    [mod] 必要最低限のライブラリで動くように修正

--- WeatherReport.py
+++ WeatherReport.py
@@ -1,82 +1,72 @@
-import requests
 import time
-# import urllib
+import urllib.request, urllib.parse
 import json
 import datetime
-import os
 from bs4 import BeautifulSoup as bs4
-# from janome.tokenizer import Tokenizer
-import urllib.request, urllib.parse
+
+# 場所から緯度経度を取得
+def getLatitude(place):
+
+    params = {
+        'q': place
+    }
+
+    params = urllib.parse.urlencode(params)
+
+    url = 'http://www.geocoding.jp/api/?' + params
+
+    # スクレイピング用にURLを指定
+    url = urllib.request.urlopen(url).read()
+    soup = bs4(url, 'html.parser')
+
+    if soup.find('error'):
+        return '', ''
+
+    lat = soup.find('lat').string
+    lon = soup.find('lng').string
+
+    # 緯度経度取得APIの規定で10秒待つ
+    time.sleep(5)
+
+    return lat, lon
+
+# 経度緯度から天気予報を取得
+def getWeatherReport(lat, lon):
+    baseUrl =  'https://map.yahooapis.jp/weather/V1/place?coordinates='
+    appId = 'dj00aiZpPWNxa0FvMWwyOWxIciZzPWNvbnN1bWVyc2VjcmV0Jng9NDg-'
+
+    url = baseUrl + lon + ',' + lat + '&output=json&appid=' + appId
+
+    tmp = urllib.request.urlopen(url).read()
+
+    json_tree = json.loads(tmp)
+
+    weatherList = json_tree['Feature'][0]['Property']['WeatherList']['Weather']
+
+    for weather in weatherList:
+        dt = datetime.datetime.strptime(weather['Date'], '%Y%m%d%H%M')
+        dt = dt.strftime("%Y/%m/%d %H:%M")
+
+        if weather['Rainfall'] == 0.0:
+            print(dt + ': 傘は必要ありません')
+        elif weather['Rainfall'] >= 0.5:
+            print(dt + ': 長時間出かける場合は傘を持っていきましょう')
+        elif weather['Rainfall'] >= 1.0:
+            print(dt + ': 絶対に傘を持って出かけてください')
+
+    return weatherList
 
 
-def lambda_handler(request, context):
-    for event in json.loads(request['body'])['events']:
+def main():
+    print('どこの天気を知りたい？')
 
-        place = event['message']['text']
+    text = input('>> ')
 
-        # tokenizer = Tokenizer()
-        # place = ''
+    # 天気予報取得のために緯度、経度を取得
+    lat, lon = getLatitude(text)
 
-        # # 引数で与えられたテキストを形態素解析し、地域を指定
-        # for token in tokenizer.tokenize(text):
-        #     p = token.part_of_speech.split(',')
-        #     if '地域' in p:
-        #         place += token.surface
+    # 緯度、経度を用いて天気予報を取得
+    weatherReport = getWeatherReport(lat, lon)
 
-        placeUrl = 'http://www.geocoding.jp/api/?q=' + place
-
-        # スクレイピング用にURLを指定
-        url = requests.get(placeUrl)
-        soup = bs4(url.content, 'html.parser')
-
-        if soup.find('error'):
-            return '', ''
-
-        lat = soup.find('lat').string
-        lon = soup.find('lng').string
-
-        # 緯度経度取得APIの規定で10秒待つ
-        time.sleep(5)
-
-        baseUrl =  os.environ['BASE_URL']
-        appId = os.environ['APP_ID']
-
-        url = baseUrl + lon + ',' + lat + '&output=json&appid=' + appId
-
-        tmp = urllib.request.urlopen(url).read()
-
-        json_tree = json.loads(tmp)
-
-        weatherList = json_tree['Feature'][0]['Property']['WeatherList']['Weather']
-
-        for weather in weatherList:
-            dt = datetime.datetime.strptime(weather['Date'], '%Y%m%d%H%M')
-            dt = dt.strftime("%Y/%m/%d %H:%M")
-
-            if weather['Rainfall'] == 0.0:
-                message = dt + ': 傘は必要ありません'
-            elif weather['Rainfall'] > 0.0 and weather['Rainfall'] < 1.0:
-                message = dt + ': 長時間出かける場合は傘を持っていきましょう'
-            elif weather['Rainfall'] >= 1.0:
-                message = dt + ': 絶対に傘を持って出かけてください'
-
-        url = 'https://api.line.me/v2/bot/message/reply'
-        headers = {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + os.environ['ChannelAccessToken']
-        }
-        body = {
-            'replyToken': event['replyToken'],
-            'messages': [
-                {
-                    "type": "text",
-                    "text": message,
-                }
-            ]
-        }
-
-        req = urllib.request.Request(url, data=json.dumps(body).encode('utf-8'), method='POST', headers=headers)
-        with urllib.request.urlopen(req) as res:
-            res.read().decode("utf-8")
-
-    return {'statusCode': 200, 'body': '{}'}
+if __name__ == '__main__':
+    main()
